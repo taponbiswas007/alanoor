2ce1e651dc0f0da8ec03155b779fe7509c42afda
taponbiswas007 <taponbiswassdp@gmail.com>
2023-06-01 19:54:46 +0600

    banner update

--- js/main.js
+++ js/main.js
@@ -3,3 +3,19 @@ $("document").ready(function(){
         $(".pricelist").slideToggle();
     });
 });
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
