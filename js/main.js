2cf27cb45aec9e63e7b293bfbcb719ce2aca314c
taponbiswas007 <taponbiswassdp@gmail.com>
2023-07-25 00:11:56 +0600

    slide

--- js/main.js
+++ js/main.js
@@ -1,25 +1,24 @@
 $("document").ready(function(){
-    $(".price-list").click(function(){
-        $(".pricelist").slideToggle();
-    });
     $(".toggle-bar").click(function(){
-      $(".navitem").slideToggle();
+      $(".navitem-area").slideToggle();
+      $(".cross-mark").toggle();
+      $(".bars").toggle();
 
     });
 });
 var swiper = new Swiper(".mySwiper", {
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
