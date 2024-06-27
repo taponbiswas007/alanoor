c93322c1cb460a84bf878120de580e0bbb9ea819
taponbiswas007 <taponbiswassdp@gmail.com>
2024-06-27 18:36:13 +0600

    klfjsafjslk

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
   $("#theme-toggle").click(function(){
-    if ($("#themeMode").hasClass("light-theme")) {
-      $("#themeMode").removeClass("light-theme").addClass("dark-theme");
-    } else {
-      $("#themeMode").removeClass("dark-theme").addClass("light-theme");
-    }
+      if ($("#themeMode").hasClass("light-theme")) {
+          $("#themeMode").removeClass("light-theme").addClass("dark-theme");
+      } else {
+          $("#themeMode").removeClass("dark-theme").addClass("light-theme");
+      }
   });
 });
