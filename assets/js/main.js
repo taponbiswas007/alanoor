b9be6f991d20044ed07b7373ddd9a1f008e08cd1
taponbiswas007 <taponbiswassdp@gmail.com>
2024-06-27 19:22:29 +0600

    dark mode update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,12 @@
-$(document).ready(function(){
-  $("#theme-toggle").click(function(){
-      if ($("#themeMode").hasClass("light-theme")) {
-          $("#themeMode").removeClass("light-theme").addClass("dark-theme");
-      } else {
-          $("#themeMode").removeClass("dark-theme").addClass("light-theme");
-      }
+$(document).ready(function () {
+  $("#theme-toggle").click(function () {
+    if ($("#themeMode").hasClass("light-theme")) {
+      $("#themeMode").removeClass("light-theme").addClass("dark-theme");
+    } else {
+      $("#themeMode").removeClass("dark-theme").addClass("light-theme");
+    }
+  });
+  $('#theme-toggle').on('click', function () {
+    $('body').toggleClass('dark-mode');
   });
 });
