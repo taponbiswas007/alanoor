cc250512194516e4bb7e50bac2967d2afae948b4
taponbiswas007 <taponbiswassdp@gmail.com>
2024-06-29 12:51:07 +0600

    fljksadjlfsjadkf

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
   $("#theme-toggle").click(function () {
-    if ($("#themeMode").hasClass("light-theme")) {
-      $("#themeMode").removeClass("light-theme").addClass("dark-theme");
+    if ($(".themeMode").hasClass("light-theme")) {
+      $(".themeMode").removeClass("light-theme").addClass("dark-theme");
     } else {
-      $("#themeMode").removeClass("dark-theme").addClass("light-theme");
+      $(".themeMode").removeClass("dark-theme").addClass("light-theme");
     }
   });
   $('#theme-toggle').on('click', function () {
