e0be30b7cba0e4bfbc34918cac5ae63475d0b538
taponbiswas007 <taponbiswassdp@gmail.com>
2024-06-29 17:37:27 +0600

    faslfslfjs

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,3 +10,44 @@ $(document).ready(function () {
     $('body').toggleClass('dark-mode');
   });
 });
+
+$(document).ready(function() {
+    
+  $('.slider-for').slick({
+     slidesToShow: 1,
+     slidesToScroll: 1,
+     arrows: false,
+     //fade: true,
+     asNavFor: '.slider-nav'
+   });
+   $('.slider-nav').slick({
+     slidesToShow: 5,
+     slidesToScroll: 1,
+     asNavFor: '.slider-for',
+     dots: false,
+     centerMode: true,
+     focusOnSelect: true,
+     spaceBetween: 10,
+     responsive: [
+         {
+           breakpoint: 992,
+           settings: {
+             slidesToShow: 4
+           }
+         },
+         {
+           breakpoint: 768,
+           settings: {
+             slidesToShow: 3
+           }
+         },
+         {
+           breakpoint: 480,
+           settings: {
+             slidesToShow: 2
+           }
+         }
+       ]
+   });
+ });
+ 
